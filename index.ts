e3da20118fd6e769f0ccc9b7b537f60fef7acccb
Md. Habibur Rahaman <34125799+nobel7761@users.noreply.github.com>
2023-10-11 02:23:29 +0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
-import { NextFunction, Request, Response } from "express";
-
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 const uri =
-  "mongodb+srv://pc-builder-admin:UoTTpB2gqPzTY0RO@cluster0.grizk.mongodb.net/pc-builder?retryWrites=true&w=majority";
+  "mongodb+srv://pc-builder-admin:di0EQSSG2dWaVY9T@cluster0.grizk.mongodb.net/pc-builder?retryWrites=true&w=majority";
 
 app.use(express.json());
 
@@ -15,45 +13,51 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-app.get(
-  "/products",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await client.connect();
-      const productsCollection = client.db("pc-builder").collection("products");
+app.get("/products", async (req, res, next) => {
+  try {
+    await client.connect();
+    const database = client.db("pc-builder");
+    const productsCollection = database.collection("products");
 
-      const products = await productsCollection.find({}).toArray();
-      res.status(200).send({ message: "Success", status: 200, data: products });
-    } catch (error) {
-      console.log("error", error);
-      res.status(500).json({ message: "Internal server error" });
+    const category = req.query.category;
+
+    const filter = category
+      ? { category: { $regex: new RegExp(category, "i") } }
+      : {};
+
+    const products = await productsCollection.find(filter).toArray();
+
+    if (products) {
+      res.status(200).json(products);
+    } else {
+      res.status(404).json({ message: "Products not found" });
     }
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Something went wrong!" });
   }
-);
+});
 
-app.get(
-  "/product/:id",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await client.connect();
-      const productsCollection = client.db("pc-builder").collection("products");
+app.get("/product/:id", async (req, res, next) => {
+  try {
+    await client.connect();
+    const database = client.db("pc-builder");
+    const productsCollection = database.collection("products");
 
-      //   const productId = req.query.id as string;
-      const product = await productsCollection.findOne({
-        _id: new ObjectId(req.params.id),
-      });
+    const product = await productsCollection.findOne({
+      _id: new ObjectId(req.params.id),
+    });
 
-      if (product) {
-        res.status(200).json({ message: "Success", data: product });
-      } else {
-        res.status(404).json({ message: "Product not found" });
-      }
-    } catch (error) {
-      console.log("error", error);
-      res.status(500).json({ message: "Internal server error" });
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
     }
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Internal server error" });
   }
-);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
